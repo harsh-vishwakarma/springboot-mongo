dd1af7050c39b500c767212bf5677298f787d642
Harsh Vishwakarma <inbox.harshy@gmail.com>
2023-09-14 21:32:42 +0530

    Multipart file upload and Multiple file upload test

--- src/main/java/com/springtest/demoapp/controller/EmployeeController.java
+++ src/main/java/com/springtest/demoapp/controller/EmployeeController.java
@@ -1,8 +1,14 @@
 package com.springtest.demoapp.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +16,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springtest.demoapp.dao.EmployeRepositoryM;
 import com.springtest.demoapp.entities.EmployeeMongo;
@@ -19,14 +28,15 @@ import com.springtest.demoapp.entities.EmployeeMongo;
 @RequestMapping("/employee")
 public class EmployeeController {
 
+    public final String UPLOAD_PATH = new ClassPathResource("static/images").getFile().getAbsolutePath();
 
-
+    public EmployeeController() throws IOException {
+        
+    }
     @Autowired
     private EmployeRepositoryM employeRepositoryMongo;
 
 
-
-    
     // @GetMapping("/{id}/")
     // public Employee getEmployee(@PathVariable(value = "id") int id,  HttpServletResponse response){
     //     Employee employee = employeeRepository.findById(id);
@@ -52,5 +62,26 @@ public class EmployeeController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         return ResponseEntity.of(employee);
+    }
+
+    @PostMapping("/upload/")
+    public ResponseEntity<String> fileUpload(@RequestParam MultipartFile file){
+        System.out.println(file.getOriginalFilename());
+        System.out.println(UPLOAD_PATH);
+        try {
+            Files.copy(file.getInputStream(), Path.of(UPLOAD_PATH, File.separator, file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/images/").path(file.getOriginalFilename()).toUriString());
+    }
+
+    @PostMapping("/multi-upload/")
+    public ResponseEntity<String> fileUpload(@RequestParam MultipartFile[] files){
+        for (MultipartFile multipartFile : files) {
+            System.out.println(multipartFile.getOriginalFilename());
+        }
+        // System.out.println(file.getOriginalFilename());
+        return ResponseEntity.ok("uploaded");
     }
 }
